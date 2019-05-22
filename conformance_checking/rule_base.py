a01144293d2a934b3e57c3febbd8d6cec800ab40
sim-o-n <remysimo@hu-berlin.de>
2019-05-22 20:51:15 +0200

    rework rules

--- conformance_checking/rule_base.py
+++ conformance_checking/rule_base.py
@@ -1,165 +1,197 @@
-
 class Rule_Checker():
 
-    def get_percentage(self, total: int, observations: int) -> float:
-        return (100 / total) * observations
+	def get_percentage(self, total: int, observations: int) -> float:
+		return round((100 / total) * observations, 2)
+
+	def check_cardinality(self, log, activity: str, upper: int, lower: int) \
+			-> \
+			dict:
+		"""
+
+		:param log: event log
+		:param activity: name of the activity
+		:param upper: max. number of occurrence in one trace, -1 if infinite
+		:param lower: min. number of occurrence in one trace
+		:return: report
+		"""
+
+		violation_upper = 0
+		violation_lower = 0
+
+		for trace in log:
+
+			events = trace['events']
+			counter = events.count(activity)
+
+			if counter < lower:
+				violation_lower += 1
+			elif counter > upper != -1:
+				violation_upper += 1
+
+		return {'activity': activity,
+				'violation upper': (violation_upper,
+									self.get_percentage(len(log),
+														violation_upper)),
+				'violation lower': (violation_lower,
+									self.get_percentage(len(log),
+														violation_lower))}
+
+	def check_order(self, log, first: str, second: str) -> dict:
+		"""
+
+		:param log: event log
+		:param first: activity
+		:param second: activity
+		:return: report
+		"""
+
+		violations = 0
+		traces = 0
+
+		for trace in log:
+			events = trace['events']
+			first_stack = []
+
+			if first not in events or second not in events:
+				break
+
+			traces += 1
+
+			for event in events:
+				if event == first:
+					first_stack.append(event)
+				elif event == second and len(first_stack) == 0:
+					violations += 1
+
+		return {'first': first, 'second': second,
+				'violations': (violations,
+							   self.get_percentage(traces, violations))}
+
+	def check_response(self, log, request: str, response: str,
+					   single_occurrence=False) -> dict:
+		"""
+
+		:param log: event log
+		:param request: activity which expects a requested activity
+		:param response: requested activity
+		:param single_occurrence: specifies whether a single occurrence of the
+		responding activity already satisfies the rule
+		:return: report
+		"""
+
+		violations = 0
+		traces = 0
+		violated_traces = 0
 
 
-    def check_cardinality(self, log, activity: str, upper: int, lower: int) -> \
-            dict:
-        """
+		for trace in log:
+			events = trace['events']
+			req_stack = []
 
-        :param log: event log
-        :param activity: name of the activity
-        :param upper: max. number of occurrence in one trace, -1 if infinite
-        :param lower: min. number of occurrence in one trace
-        :return: report
-        """
+			tracked = False
 
-        violation_upper = 0
-        violation_lower = 0
-
-        for trace in log:
-
-            events = trace['events']
-            counter = events.count(activity)
-
-            if counter < lower:
-                violation_lower += 1
-            elif counter > upper != -1:
-                violation_upper += 1
-
-        return {'activity': activity,
-                'violation upper': (violation_upper,
-                                    self.get_percentage(len(log), violation_upper)),
-                'violation lower': (violation_lower,
-                                    self.get_percentage(len(log), violation_lower))}
+			if not single_occurrence:
+				for event in events:
+					if event == request:
+						req_stack.append(event)
+						if not tracked:
+							traces += 1
+							tracked = True
+					elif event == response and len(req_stack) > 0:
+						req_stack.pop()
 
 
-    def check_order(self, log, first: str, second: str) -> dict:
-        """
+				if len(req_stack) > 0:
+					violations += len(req_stack)
+					violated_traces += 1
+			else:
+				if request in events and response in events:
+					traces += 1
+					req_idx = events[::-1].index(request)
+					res_idx = events[::-1].index(response)
 
-        :param log: event log
-        :param first: activity
-        :param second: activity
-        :return: report
-        """
-
-        violations = 0
-        traces = 0
-
-        for trace in log:
-            events = trace['events']
-            first_stack = []
-
-            if first not in events or second not in events:
-                break
-
-            traces += 1
-
-            for event in events:
-                if event == first:
-                    first_stack.append(event)
-                elif event == second and len(first_stack) == 0:
-                    violations += 1
-
-        return {'first': first, 'second': second,
-                'violations': (violations,
-                               self.get_percentage(traces, violations))}
+					if req_idx < res_idx:
+						violations += 1
+						violated_traces += 1
+				elif request in events and response not in events:
+					traces += 1
+					violated_traces += 1
+					violations += 1
 
 
-    def check_response(self, log, request: str, response: str,
-                       single_occurrence=False) -> dict:
-        """
-
-        :param log: event log
-        :param request: activity which expects a requested activity
-        :param response: requested activity
-        :param single_occurrence: specifies whether a single occurrence of the
-        responding activity already satisfies the rule
-        :return: report
-        """
-
-        violations = 0
-        traces = 0
-        violated_traces = 0
-
-        for trace in log:
-            events = trace['events']
-
-            req_count = events.count(request)
-            res_count = events.count(response)
-
-            if req_count > 0:
-                traces += 1  # relevant traces
-                if req_count > res_count and not single_occurrence:
-                    violated_traces += 1  # violated traces
-                    violations += req_count - res_count
-                elif single_occurrence and res_count == 0:
-                    violated_traces += 1  # violated traces
-                    violations += 1
-
-        return {'request': request, 'response': response,
-                'violations': (violations, violated_traces,
-                               self.get_percentage(traces, violated_traces))}
+		return {'request': request, 'response': response,
+			'violations': (violations, violated_traces,
+						   self.get_percentage(traces, violated_traces))}
 
 
-    def check_precedence(self, log, preceding: str, request: str,
-                         single_occurrence=False) -> dict:
-        """
+	def check_precedence(self, log, preceding: str, request: str,
+						 single_occurrence=False) -> dict:
+		"""
 
-        :param log: event log
-        :param preceding: activity that should precede the requesting activity
-        :param request: requesting activity
-        :param single_occurrence: specifies whether a single occurrence of the
-        preceding activity already satisfies the rule
-        :return: report
-        """
+		:param log: event log
+		:param preceding: activity that should precede the requesting activity
+		:param request: requesting activity
+		:param single_occurrence: specifies whether a single occurrence of the
+		preceding activity already satisfies the rule
+		:return: report
+		"""
 
-        violations = 0
-        traces = 0
-        violated_traces = 0
+		violations = 0
+		traces = 0
+		violated_traces = 0
 
-        for trace in log:
-            events = trace['events']
+		for trace in log:
+			events = trace['events']
+			pre_stack = []
 
-            pre_count = events.count(preceding)
-            req_count = events.count(request)
+			tracked = False
+			violated_trace_tracked = False
 
-            if req_count > 0:
-                traces += 1
-                if req_count > pre_count and not single_occurrence:
-                    violated_traces += 1
-                    violations += req_count - pre_count
-                elif single_occurrence and pre_count == 0:
-                    violated_traces += 1  # violated traces
-                    violations += 1
+			for event in events:
+				if event == preceding:
+					pre_stack.append(event)
+				elif event == request and len(pre_stack) > 0:
+					if not single_occurrence:
+						pre_stack.pop()
+					if not tracked:
+						traces += 1
+						tracked = True
+				elif event == request:
+					violations += 1
+					if not violated_trace_tracked:
+						violated_traces += 1
+						violated_trace_tracked = True
+					if not tracked:
+						traces += 1
+						tracked = True
 
-        return {'preceding': preceding, 'request': request,
-                'violations': (violations, violated_traces,
-                               self.get_percentage(traces, violated_traces))}
+
+		return {'preceding': preceding, 'request': request,
+				'violations': (violations, violated_traces,
+							   self.get_percentage(traces, violated_traces))}
 
 
-    def check_exclusive(self, log, first_activity: str, second_activity: str) \
-            -> dict:
-        """
+	def check_exclusive(self, log, first_activity: str, second_activity: str) \
+			-> dict:
+		"""
 
-        :param log: event log
-        :param first_activity: activity
-        :param second_activity: activity
-        :return: report
-        """
+		:param log: event log
+		:param first_activity: activity
+		:param second_activity: activity
+		:return: report
+		"""
 
-        violations = 0
-        violated_traces = 0
+		violations = 0
+		violated_traces = 0
 
-        for trace in log:
-            events = trace['events']
+		for trace in log:
+			events = trace['events']
 
-            if first_activity in events and second_activity in events:
-                violated_traces += 1
-                violations += 1
+			if first_activity in events and second_activity in events:
+				violated_traces += 1
+				violations += 1
 
-        return {'first activity': first_activity, 'second activity': second_activity,
-                'violations': (violations,
-                               self.get_percentage(len(log), violated_traces))}
+		return {'first activity': first_activity,
+				'second activity': second_activity,
+				'violations': (violations,
+							   self.get_percentage(len(log), violated_traces))}
--- conformance_checking/test_rule_Checker.py
+++ conformance_checking/test_rule_Checker.py
@@ -11,12 +11,16 @@ class TestRule_Checker(TestCase):
 				{'trace': 1, 'events': ['A', 'B', 'D', 'C', 'E']},
 				{'trace': 2, 'events': ['A', 'B', 'D', 'B', 'F']},
 				{'trace': 3, 'events': ['B', 'C', 'E']},
-				{'trace': 1, 'events': ['B', 'E']}
+				{'trace': 1, 'events': ['B', 'E']},
+				{'trace': 1,
+				 'events': ['A', 'B', 'C', 'E', 'C', 'D']}, # single pre
+				{'trace': 1, 'events': ['A', 'C', 'B', 'E', 'C', 'D']},
+				{'trace': 1, 'events': ['G', 'G', 'E', 'T', 'G']},
 			]
 
 	def test_get_percentage(self):
 		res = self.rc.get_percentage(450, 20)
-		self.assertEqual(round(res, 2), 4.44)
+		self.assertEqual(res, 4.44)
 
 	def test_check_cardinality(self):
 		res = self.rc.check_cardinality(self.log, 'A', 1, 0)
@@ -34,14 +38,23 @@ class TestRule_Checker(TestCase):
 
 	def test_check_response(self):
 		res = self.rc.check_response(self.log, 'B', 'E')
-		self.assertEqual(res['violations'], (2, 1, 25.0))
+		self.assertEqual(res['violations'], (2, 1, 16.67))
 
 		res = self.rc.check_response(self.log, 'A', 'D')
 		self.assertEqual(res['violations'], (0, 0, 0.0))
 
+		res = self.rc.check_response(self.log, 'G', 'T', True)
+		self.assertEqual(res['violations'], (1, 1, 100.0))
+
+		res = self.rc.check_response(self.log, 'G', 'T')
+		self.assertEqual(res['violations'], (2, 1, 100.0))
+
 	def test_check_precedence(self):
 		res = self.rc.check_precedence(self.log, 'B', 'C')
-		self.assertEqual(res['violations'], (0, 0, 0.0))
+		self.assertEqual(res['violations'], (2, 2, 50.0))
+
+		res = self.rc.check_precedence(self.log, 'B', 'C', True)
+		self.assertEqual(res['violations'], (1, 1, 25.0))
 
 	def test_check_exclusive(self):
 		res = self.rc.check_exclusive(self.log, 'E', 'F')
